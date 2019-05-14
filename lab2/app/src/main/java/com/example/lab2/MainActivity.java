72375479e20c216a20c46b421a8922fe44591888
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-14 08:34:08 -0400

    Lab2 initial commit

---
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText loginEditText = (EditText) findViewById(R.id.loginEditText);
+                Toast.makeText(getApplicationContext(), "Username: ", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}

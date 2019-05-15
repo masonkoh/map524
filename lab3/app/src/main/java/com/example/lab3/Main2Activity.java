7bee2e8ab721b6ec4f5bce5432e77f6fae5e3c9d
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-15 12:51:12 -0400

    username intent done

--- lab3/app/src/main/java/com/example/lab3/Main2Activity.java
+++ lab3/app/src/main/java/com/example/lab3/Main2Activity.java
@@ -1,7 +1,12 @@
 package com.example.lab3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+        String tempString = intent.getStringExtra("firstExtra");
+
+
+        Button homeBtn = (Button) findViewById(R.id.homeBtn);
+
+
+        TextView usernameIsTextView = findViewById(R.id.usernameIsTextView);
+        usernameIsTextView.setText("username is " + tempString);
+
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +17,16 @@ public class MainActivity extends AppCompatActivity {
         Button usernameBtn = (Button) findViewById(R.id.usernameBtn);
         Button passwordBtn = (Button) findViewById(R.id.passwordBtn);
         Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        final EditText IDEditText = (EditText) findViewById(R.id.IDEditText);
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
 
         usernameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("firstExtra", IDEditText.getText().toString());
                 startActivity(intent);
+
             }
         });
         passwordBtn.setOnClickListener(new View.OnClickListener() {

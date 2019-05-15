477dd3bfc6df3cfdf244a25fdf6b1f7aabff3465
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-15 11:26:41 -0400

    Buttons added in main java file

--- lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.lab3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button usernameBtn = (Button) findViewById(R.id.usernameBtn);
+        Button passwordBtn = (Button) findViewById(R.id.passwordBtn);
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+
+        
     }
 }

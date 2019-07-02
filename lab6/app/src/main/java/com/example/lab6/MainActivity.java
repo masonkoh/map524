c98f0fdb7ce4da4c376b4a4417399f04a447a4d2
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-07-02 18:56:06 -0400

    lab6 done

--- lab6/app/src/main/java/com/example/lab6/MainActivity.java
+++ lab6/app/src/main/java/com/example/lab6/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lab6;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("mkTag", "SQL(sqLiteDatabase1) created!!!!@#!@#");
         final EditText editText_id = findViewById(R.id.edittext_id);
         final EditText editText_name = findViewById(R.id.edittext_name);
-        final EditText edittext_marks = findViewById(R.id.edittext_marks);
+        final EditText editText_marks = findViewById(R.id.edittext_marks);
         Button button_add = findViewById(R.id.button_add);
         Button button_view = findViewById(R.id.button_view);
         Button button_find = findViewById(R.id.button_find);
@@ -32,10 +33,11 @@ public class MainActivity extends AppCompatActivity {
         button_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sqLiteDatabase1.execSQL("INSERT INTO IDNameGrade VALUES('"+ editText_id.getText() + "','"+ editText_name.getText() + "','"+edittext_marks.getText()+"');");
+                sqLiteDatabase1.execSQL("INSERT INTO IDNameGrade VALUES('" + editText_id.getText() + "','" + editText_name.getText() + "','" + editText_marks.getText() + "');");
+                Log.d("mkTag", "data adding");
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
                 alertDialog.setTitle("The following student was added");
-                alertDialog.setMessage("ID: " + editText_id.getText() + " Name: " + editText_name.getText() + " Marks: " + edittext_marks.getText());
+                alertDialog.setMessage("ID: " + editText_id.getText() + " Name: " + editText_name.getText() + " Marks: " + editText_marks.getText());
                 // for close button
                 alertDialog.setNeutralButton("CLOSE", new DialogInterface.OnClickListener() {
                     @Override
@@ -46,8 +48,101 @@ public class MainActivity extends AppCompatActivity {
                 alertDialog.show();
                 editText_id.setText("");
                 editText_name.setText("");
-                edittext_marks.setText("");
+                editText_marks.setText("");
             }
         });
+
+        button_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuffer content = new StringBuffer();
+                Cursor cursorSelectAll = sqLiteDatabase1.rawQuery("SELECT * FROM IDNameGrade", null);
+                String tempTitle = "No student records";
+
+                while (cursorSelectAll.moveToNext()) {
+                    content.append("ID: " + cursorSelectAll.getString(0) + " Name: " + cursorSelectAll.getString(1) + " Marks: " + cursorSelectAll.getString(2) + ".\n");
+                    tempTitle = "The following students have been added";
+                }
+
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle(tempTitle);
+                alertDialog.setMessage(content);
+
+                alertDialog.setNeutralButton("CLOSE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                alertDialog.show();
+
+
+            }
+        });
+
+        button_find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuffer content = new StringBuffer();
+                Cursor cursorSelectAll = sqLiteDatabase1.rawQuery("SELECT * FROM IDNameGrade WHERE ID = '" + editText_id.getText() + "';", null);
+                String tempTitle = "This student doesn't exists";
+
+                while (cursorSelectAll.moveToNext()) {
+                    content.append("ID: " + cursorSelectAll.getString(0) + " Name: " + cursorSelectAll.getString(1) + " Marks: " + cursorSelectAll.getString(2) + ".\n");
+                    tempTitle = "Students details are as follows";
+                }
+
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle(tempTitle);
+                alertDialog.setMessage(content);
+                alertDialog.setNeutralButton("CLOSE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                alertDialog.show();
+                editText_id.setText("");
+                editText_name.setText("");
+                editText_marks.setText("");
+            }
+        });
+
+        button_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuffer content = new StringBuffer();
+                Cursor cursorSelectAll = sqLiteDatabase1.rawQuery("SELECT * FROM IDNameGrade WHERE ID = '" + editText_id.getText() + "';", null);
+                String tempTitle = "This student deosn't exist";
+
+                while (cursorSelectAll.moveToNext()) {
+                    content.append("ID: " + cursorSelectAll.getString(0) + " Name: " + cursorSelectAll.getString(1) + " Marks: " + cursorSelectAll.getString(2) + "\n");
+                    tempTitle = "The following student has been deleted";
+                }
+
+
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle(tempTitle);
+                alertDialog.setMessage(content);
+                alertDialog.setNeutralButton("CLOSE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                alertDialog.show();
+
+                Cursor cursorSelectDelete = sqLiteDatabase1.rawQuery("DELETE FROM IDNameGrade WHERE ID = '" + editText_id.getText() + "';", null);
+                while (cursorSelectDelete.moveToNext()) {
+                    content.append("ID: " + cursorSelectDelete.getString(0) + " Name: " + cursorSelectDelete.getString(1) + " Marks: " + cursorSelectDelete.getString(2) + ".\n");
+                }
+
+                editText_id.setText("");
+                editText_name.setText("");
+                editText_marks.setText("");
+            }
+        });
+
+
     }
 }

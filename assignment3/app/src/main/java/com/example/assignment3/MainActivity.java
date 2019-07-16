8b0a6016cd7effd1f440e9cb592171c844173e31
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-07-16 11:15:31 -0400

    main_activity.xml done

---
+++ assignment3/app/src/main/java/com/example/assignment3/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.assignment3;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.Button;
+        import android.widget.LinearLayout;
+        import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView;
+        Button add_btn;
+        Button exit_btn;
+        LinearLayout linearLayout;
+    }
+}

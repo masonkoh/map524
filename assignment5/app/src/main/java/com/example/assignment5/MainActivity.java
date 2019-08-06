1cebd13c3701f7488f06fb26476c67ba9bd55898
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-08-06 15:15:26 -0400

    done, in-app browser not implemented tho

--- assignment5/app/src/main/java/com/example/assignment5/MainActivity.java
+++ assignment5/app/src/main/java/com/example/assignment5/MainActivity.java
@@ -1,13 +1,96 @@
 package com.example.assignment5;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView earthquakelist_listview;
+    String earthquakeAPI_string = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=20&minmagnitude=6&orderby=time";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        earthquake_asynctask task = new earthquake_asynctask();
+        task.execute(earthquakeAPI_string);
+
     }
+
+    // https://stackoverflow.com/questions/9671546/asynctask-android-example
+    class earthquake_asynctask extends AsyncTask<String, Void, List<String>> {
+        @Override
+        protected List<String> doInBackground(String... earthquakeAPI_string) {
+            return Utils.fetchEarthquakeData(earthquakeAPI_string[0]);
+        }
+
+        public void onPostExecute(List<String> postExecuteResult) {
+            CustomListAdapter arrayAdapter = new CustomListAdapter(MainActivity.this, postExecuteResult);
+
+            earthquakelist_listview = findViewById(R.id.earthquakelist_listview);
+            earthquakelist_listview.setAdapter(arrayAdapter);
+            earthquakelist_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                }
+            });
+
+        }
+    }
+
+    class CustomListAdapter extends ArrayAdapter<String> {
+        Activity context;
+        List<String> itemname1;
+
+        public CustomListAdapter(Activity activity, List<String> itemnameA) {
+            super(activity, R.layout.earthquake_listdetail, itemnameA);
+            this.context = activity;
+            this.itemname1 = itemnameA;
+        }
+
+        public View getView(int position, View view, ViewGroup parent) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            View rowView = inflater.inflate(R.layout.earthquake_listdetail, null, true);
+            String earthInfo[] = itemname1.get(position).split("@@");
+            TextView textInfo1 = rowView.findViewById(R.id.listdetail1_textview);
+            textInfo1.setText(earthInfo[0]);
+            TextView textInfo2 = rowView.findViewById(R.id.listdetail2_textview);
+            textInfo2.setText(new Date(Long.parseLong(earthInfo[1])).toString());
+
+
+            if (position % 2 == 0) {
+                textInfo1.setBackgroundColor(Color.parseColor("#98CE00"));
+            }
+            if (position % 2 == 0) {
+                textInfo2.setBackgroundColor(Color.parseColor("#98CE00"));
+            }
+
+
+
+
+            return rowView;
+
+        }
+    }
+
 }
---
+++ assignment5/app/src/main/java/com/example/assignment5/Utils.java
@@ -0,0 +1,121 @@
+package com.example.assignment5;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class Utils {
+
+    public static List<String> fetchEarthquakeData(String earthquakeAPI_string) {
+        URL url = createURL(earthquakeAPI_string);
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.d("MKLOG< E R R O R: ", "fetchEarthquakeData: ");
+        }
+        return extractFeatureFromJson(jsonResponse);
+    }
+
+    public static URL createURL(String earthquakeAPI_string) {
+        URL url = null;
+        try {
+            url = new URL(earthquakeAPI_string);
+        } catch (MalformedURLException e) {
+            Log.d("MKLOG < E R R O R>: ", "bad url...!");
+        }
+        return url;
+    }
+
+    public static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection httpURLConnection = null;
+        InputStream inputStream = null;
+        try {
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+//            httpURLConnection.setReadTimeout(10000);
+//            httpURLConnection.setConnectTimeout(15000);
+//            httpURLConnection.setRequestMethod("GET");
+            Log.d("mklog", url.toString());
+            httpURLConnection.connect();
+            if (httpURLConnection.getResponseCode() == 200) {
+                inputStream = httpURLConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+
+            } else {
+                Log.d("MKLOG <E R R O R>: ", "makeHttpRequest: " + httpURLConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+// implement later
+        } finally {
+            if (httpURLConnection != null) {
+                httpURLConnection.disconnect();
+
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+
+    }
+
+    public static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output_StringBuilder = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader reader_InputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader_BufferedReader = new BufferedReader(reader_InputStreamReader);
+            String line_String = reader_BufferedReader.readLine();
+            while (line_String != null) {
+                output_StringBuilder.append(line_String);
+                line_String = reader_BufferedReader.readLine();
+            }
+        }
+        return output_StringBuilder.toString();
+    }
+
+    private static List<String> extractFeatureFromJson(String earthquakejson_String) {
+        if (TextUtils.isEmpty(earthquakejson_String)) {
+            return null;
+        }
+        List<String> earthquake_listString = new ArrayList<>();
+        try {
+            JSONObject baseJsonResponse = new JSONObject(earthquakejson_String);
+            JSONArray earthquake_jsonarray = baseJsonResponse.getJSONArray("features");
+            for (int i = 0; i < earthquake_jsonarray.length(); i++) {
+                JSONObject currentEarthQuake = earthquake_jsonarray.getJSONObject(i);
+                JSONObject properties = currentEarthQuake.getJSONObject("properties");
+                String title = properties.getString("title");
+                String time = properties.getString("time");
+                String url = properties.getString("url");
+                earthquake_listString.add(title + "@@" + time + "@@" + url);
+            }
+            return earthquake_listString;
+        } catch (JSONException e) {
+
+        }
+        return null;
+    }
+
+
+}
